82d978d5b2ea30c7ad7ff1547fc94c37c3d314c3
Kaylabyte <kaylabyte@tuta.io>
2025-06-21 23:36:15 +1200

    Split file
    I preemtively split the file into multiple files before it turns into a mess.

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
 import os
 
-from pytubefix import YouTube
+import videodownload
 
 # Remove tmp folder and everything in it
-def delete_tmp():
+def delete_tmp(output_dir):
     try:
-        for filename in os.listdir("./tmp"): # deletes files in folder
-            os.remove(os.path.join("./tmp", filename))
-        os.rmdir("./tmp") # deletes folder now that it is empty
+        for filename in os.listdir(output_dir): # deletes files in folder
+            os.remove(os.path.join(output_dir, filename))
+        os.rmdir(output_dir) # deletes folder now that it is empty
     except FileNotFoundError:
         pass
 
-# Display video title and streams - FOR TESTING
-def video_display_info(yt):
-    print(yt.title)
-    print("\nLOW RESOLUTION:")
-    [print(x) for x in yt.streams.filter(res="144p")]
-    print("\nAUDIO TRACKS:")
-    [print(x) for x in yt.streams.filter(only_audio=True)]
+if __name__ == "__main__":
+    output_dir = "./tmp"
+    delete_tmp(output_dir) # ensures no additional files affect anything
 
-# Download low quality video and audio
-def video_download(yt):
-    print("Downloading Video...")
-    yt.streams.filter(res="144p", mime_type="video/mp4", only_video=True).first().download(output_path="./tmp", filename="video.mp4")
-    yt.streams.filter(mime_type="audio/mp4", only_audio=True).first().download(output_path="./tmp", filename="audio.m4a")
-
-delete_tmp() # ensures no additional files affect anything
-yt = YouTube("https://www.youtube.com/watch?v=FtutLA63Cp8")
-video_display_info(yt)
-video_download(yt)
+    yt_url = "https://www.youtube.com/watch?v=FtutLA63Cp8"
+    videodownload.video_download(yt_url, output_dir)
---
+++ videodownload.py
@@ -0,0 +1,31 @@
+from pytubefix import YouTube
+
+# Display video title and streams - FOR TESTING
+def video_display_info(yt_url):
+    yt = YouTube(yt_url)
+    print(yt.title)
+    print("\nLOW RESOLUTION:")
+    [print(x) for x in yt.streams.filter(res="144p")]
+    print("\nAUDIO TRACKS:")
+    [print(x) for x in yt.streams.filter(only_audio=True)]
+
+# Download low quality video and audio
+def video_download(yt_url, output_dir):
+    yt = YouTube(yt_url)
+    print("Downloading Video...")
+    yt.streams.filter(res="144p", mime_type="video/mp4", only_video=True).first().download(output_path=output_dir, filename="video.mp4")
+    yt.streams.filter(mime_type="audio/mp4", only_audio=True).first().download(output_path=output_dir, filename="audio.m4a")
+
+if __name__ == "__main__": # for me :D
+    yt_url = "https://www.youtube.com/watch?v=FtutLA63Cp8"
+    test_streams = input("Test streams? [y/N]: ")
+    test_download = input("Test download? [y/N]:")
+
+    if test_streams == "y":
+        video_display_info(yt_url)
+
+    if test_download == "y":
+        output_dir = "testing_folder"
+        video_download(yt_url, output_dir)
+
+    print("Test(s) complete!")
